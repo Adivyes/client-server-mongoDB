614d3366998f0a62c5ddae905dad63bde90d4435
Adivyes <Adivyes@gmail.com>
2021-04-13 19:20:55 +0300

    init

--- client/src/App.js
+++ client/src/App.js
@@ -16,4 +16,8 @@ function App() {
   );
 }
 
+export function averageCalc(num1 , num2, num3, num4, num5){
+let sumUp =   num1 + num2 + num3 + num4 + num5
+return sumUp/5
+}
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { App, averageCalc} from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+test('shold return avg of 5 numbers', ()=>{
+  expect(averageCalc(1,1,1,1,1)).toBe(1)
+})
